e9647ff9c99ddbac6c588d9aa3f5d81b16c91039
theandersonn <theandersonn@gmail.com>
2019-09-05 09:21:48 -0300

    Add 2-animacao b

--- 2-animacao/app.js
+++ 2-animacao/app.js
@@ -1,8 +1,11 @@
 // Exemplo 1
-$("a").click(function(){
-    $(".modal").animate({
-        opacity: "toggle"
-    }, 500);
-});
+// $("a").click(function(){
+//     $(".modal").animate({
+//         opacity: "toggle"
+//     }, 500);
+// });
 
 // Exemplo 2
+$("a").click(function(){
+    $(".modal").slideToggle(300);
+});
