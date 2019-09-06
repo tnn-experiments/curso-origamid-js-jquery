b439ae6a47865f810b6e99254a0c12f7ba31f52b
theandersonn <theandersonn@gmail.com>
2019-09-06 11:10:33 -0300

    Add 4-tabs parte2

--- 4-tabs/js/app.js
+++ 4-tabs/js/app.js
@@ -1,9 +1,29 @@
-$(".tab-menu a").click(function(e) {
-    e.preventDefault();
-    var itemId = $(this).attr("href");
+$(document).ready(function() {
+    var classActive = "active";
+
+    $(".animais .tab-menu a").first().addClass(classActive);
+    $(".animais .item").first().addClass(classActive);
     
-    $(".tab-menu a").removeClass("active");
-    $(".item").removeClass("active");
-    $(this).addClass("active");
-    $(itemId).addClass("active");
+    $(".animais .tab-menu a").click(function(e) {
+        e.preventDefault();
+        var itemId = $(this).attr("href");
+        
+        $(".animais .tab-menu a").removeClass(classActive);
+        $(".animais .item").removeClass(classActive);
+        $(this).addClass(classActive);
+        $(itemId).addClass(classActive);
+    });  
+    
+    $(".florestas .tab-menu a").first().addClass(classActive);
+    $(".florestas .item").first().addClass(classActive);
+    
+    $(".florestas .tab-menu a").click(function(e) {
+        e.preventDefault();
+        var itemId = $(this).attr("href");
+        
+        $(".florestas .tab-menu a").removeClass(classActive);
+        $(".florestas .item").removeClass(classActive);
+        $(this).addClass(classActive);
+        $(itemId).addClass(classActive);
+    });      
 });
