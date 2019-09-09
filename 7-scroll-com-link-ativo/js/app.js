1810e3c2af5f9cad9ee590e06a75bfba926df221
theandersonn <theandersonn@gmail.com>
2019-09-09 10:05:28 -0300

    Add 7-scroll-com-link-ativo B

--- 7-scroll-com-link-ativo/js/app.js
+++ 7-scroll-com-link-ativo/js/app.js
@@ -1,3 +1,26 @@
+// tab
+$('[data-group]').each(function(){
+	var $allTarget = $(this).find('[data-target]'),
+			$allClick = $(this).find('[data-click]'),
+			activeClass = 'active';
+	
+	$allTarget.first().addClass(activeClass);
+	$allClick.first().addClass(activeClass);
+	
+	$allClick.click(function(e){
+		e.preventDefault();
+		
+		var id = $(this).data('click'),
+				$target = $('[data-target="' + id + '"]');
+		
+		$allClick.removeClass(activeClass);
+		$allTarget.removeClass(activeClass);
+		
+		$target.addClass(activeClass);
+		$(this).addClass(activeClass);
+	});
+});
+
 // scroll
 $(".menu-nav a[href^='#']").click(function(e) {
 	e.preventDefault();
@@ -24,29 +47,17 @@ $(".logo").click(function(e){
 $("section").each(function(){
 	var height = $(this).height();
 	var offsetTop = $(this).offset().top;
+	var menuHeight = $(".menu").innerHeight();
 	var id = $(this).attr("id");
 	var $itemMenu = $("a[href='#" + id + "']");
-});
 
-// tab
-$('[data-group]').each(function(){
-	var $allTarget = $(this).find('[data-target]'),
-			$allClick = $(this).find('[data-click]'),
-			activeClass = 'active';
-	
-	$allTarget.first().addClass(activeClass);
-	$allClick.first().addClass(activeClass);
-	
-	$allClick.click(function(e){
-		e.preventDefault();
-		
-		var id = $(this).data('click'),
-				$target = $('[data-target="' + id + '"]');
-		
-		$allClick.removeClass(activeClass);
-		$allTarget.removeClass(activeClass);
-		
-		$target.addClass(activeClass);
-		$(this).addClass(activeClass);
+	$(window).scroll(function() {
+		var scrollTop = $(window).scrollTop();
+
+		if(offsetTop - menuHeight < scrollTop && offsetTop + height - menuHeight > scrollTop) {
+			$itemMenu.addClass("active");
+		} else {
+			$itemMenu.removeClass("active");
+		}
 	});
 });
