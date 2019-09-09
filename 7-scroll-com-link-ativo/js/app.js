2c0a73bd68db08da05a6d4c35a4deb43facc837d
theandersonn <theandersonn@gmail.com>
2019-09-09 08:47:27 -0300

    Add 7-scroll-com-link-ativo A

---
+++ 7-scroll-com-link-ativo/js/app.js
@@ -0,0 +1,52 @@
+// scroll
+$(".menu-nav a[href^='#']").click(function(e) {
+	e.preventDefault();
+
+	var id = $(this).attr("href");
+	var menuHeight = $(".menu").innerHeight();
+	var targetOffset = $(id).offset().top;
+
+	$("html, body").animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
+// scroll to top
+$(".logo").click(function(e){
+	e.preventDefault();
+
+	$("html, body").animate({
+		scrollTop: 0
+	}, 500);
+});
+
+// section link ativo
+$("section").each(function(){
+	var height = $(this).height();
+	var offsetTop = $(this).offset().top;
+	var id = $(this).attr("id");
+	var $itemMenu = $("a[href='#" + id + "']");
+});
+
+// tab
+$('[data-group]').each(function(){
+	var $allTarget = $(this).find('[data-target]'),
+			$allClick = $(this).find('[data-click]'),
+			activeClass = 'active';
+	
+	$allTarget.first().addClass(activeClass);
+	$allClick.first().addClass(activeClass);
+	
+	$allClick.click(function(e){
+		e.preventDefault();
+		
+		var id = $(this).data('click'),
+				$target = $('[data-target="' + id + '"]');
+		
+		$allClick.removeClass(activeClass);
+		$allTarget.removeClass(activeClass);
+		
+		$target.addClass(activeClass);
+		$(this).addClass(activeClass);
+	});
+});
